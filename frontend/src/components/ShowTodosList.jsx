3f6eb0c503be1666ec4b2c00fdea71792be2434b
Ksenia Rachouti <blossomingiris@gmail.com>
2022-11-27 16:07:41 +0200

    fix: add proxy in package.json

--- frontend/src/components/ShowTodosList.jsx
+++ frontend/src/components/ShowTodosList.jsx
@@ -18,7 +18,7 @@ export function ShowTodoList({ theme }) {
         dispatch({ type: 'SET_TODOS', payload: res.data })
       })
       .catch((err) => {
-        console.log(err.message)
+        console.log(err)
       })
   }, [dispatch])
 
